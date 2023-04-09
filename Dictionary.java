98204265362c3aad1c1a355b3a9952f435571b55
BaharBlrn <96261244+BaharBlrn@users.noreply.github.com>
2023-04-09 18:53:09 +0300

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,14 +1,7 @@
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Dictionary {
     public static void main(String[] args){
+
+
 
     }
 
