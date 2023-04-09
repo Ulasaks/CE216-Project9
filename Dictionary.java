6aebdf4456309e3ad803b8cd87dde19c3b4fbbf2
BaharBlrn <96261244+BaharBlrn@users.noreply.github.com>
2023-04-09 18:54:54 +0300

    Little changes2

--- Dictionary.java
+++ Dictionary.java
@@ -1,16 +1,11 @@
 public class Dictionary {
 
-<<<<<<< HEAD
-
-
-=======
     public static void main(String[] args){
         Translation translation=new Translation();
         Word word=new Word();
         Synonym synonym=new Synonym();
 
         translation.loadDictionaries();
->>>>>>> c2a06424802b03513068d7339a659ddc0321ce7e
     }
 
 
