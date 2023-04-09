9fb7d5c47aa3fb750fd03369a4bc5447482a841c
Ulasaks <97384976+Ulasaks@users.noreply.github.com>
2023-04-09 19:04:18 +0300

    Add files via upload

--- Dictionary.java
+++ Dictionary.java
@@ -1,16 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Dictionary {
+    public static void main(String[] args) throws FileNotFoundException {
+        // Load the dictionary file
+        File dictFile = new File("dictionary.txt");
+        Scanner scanner = new Scanner(dictFile);
 
-    public static void main(String[] args){
-        Translation translation=new Translation();
-        Word word=new Word();
-        Synonym synonym=new Synonym();
+        // Parse the dictionary into a map
+        Map<String, String> dict = new HashMap<>();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split("=");
+            String word = parts[0].trim();
+            String translation = parts[1].trim();
+            dict.put(word, translation);
+        }
 
-        translation.loadDictionaries();
+        // Test the translation
+        String input = "hello";
+        String translation = dict.get(input);
+        System.out.println("Translation of " + input + ": " + translation);
     }
-
-
-
-
-
-
 }
